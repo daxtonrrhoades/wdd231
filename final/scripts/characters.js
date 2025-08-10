3a3a35a79d7d58fb954a7207718cb4a9bd2276c8
DaxtonRRhoades <daxtonrrhoades@gmail.com>
2025-08-10 15:33:09 -0600

    nearly finished characters.html

---
+++ final/scripts/characters.js
@@ -0,0 +1,52 @@
+// -----Character Cards-----
+const source = 'data/characters.json';
+
+const cards = document.querySelector('.character-cards');
+
+async function getCharacterData() {
+    const response = await fetch(source);
+    const data = await response.json();
+    // console.table(data.characters);
+    displayCharacters(data.characters);
+}
+
+getCharacterData();
+
+const displayCharacters = (characters) => {
+    characters.forEach((character) => {
+        let card = document.createElement("section");
+        let name = document.createElement("h2");
+        let portrait = document.createElement("img");
+        let desc = document.createElement('p');
+
+        portrait.setAttribute("src", character.img);
+        portrait.setAttribute("alt", `Logo for ${character.fName} ${character.lName}`);
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", 118);
+        portrait.setAttribute("height", "auto");
+
+        name.innerHTML = `${character.fName} ${character.lName}`;
+        desc.innerHTML = character.desc;
+
+        card.appendChild(name);
+        card.appendChild(portrait);
+        card.appendChild(desc);
+
+        cards.appendChild(card);
+    });
+}
+
+// -----Layout Buttons-----
+const display = document.querySelector(".character-cards");
+const gridButton = document.querySelector('#grid-btn');
+const listButton = document.querySelector('#list-btn');
+
+gridButton.addEventListener("click", () => {
+    display.classList.remove("character-list");
+    display.classList.add("character-grid");
+});
+
+listButton.addEventListener("click", () => {
+    display.classList.remove("character-grid");
+    display.classList.add("character-list");
+});
